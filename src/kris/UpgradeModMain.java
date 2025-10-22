11bb0c26da4cd12ab04da439b52c08ead2ac3e72
KrisCrossApple <167768508+KrisCrossApple@users.noreply.github.com>
2025-10-23 10:25:17 +1030

    Setting up the basics

--- src/kris/UpgradeModMain.java
+++ src/kris/UpgradeModMain.java
@@ -1,17 +1,14 @@
-package example;
+package kris;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class UpgradeModMain extends Mod{
 
-    public ExampleJavaMod(){
+    public UpgradeModMain(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
@@ -20,8 +17,8 @@ public class ExampleJavaMod extends Mod{
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                //mod sprites are prefixed with the mod name (this mod is called 'kris-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("kris-java-mod-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,7 +27,7 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading some kris content.");
     }
 
 }
